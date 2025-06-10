54d1336d2043913519c5546122f415e08a0b48cd
Pihul Lalotra <pihullalotra23@gmail.com>
2025-06-10 22:05:33 +0530

    set up authRoutes and added controller for signup route and worked a little bit on jwt

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,40 @@
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+export const signup = async (req, res) => {
+    const {fullName,email,password} = req.body
+    try {
+        //Hashing password using bcrypt
+        if(password < 6){
+            return res.status(400).json({message: "Password must be atleast 6 characters."});
+        }
+
+        const user = await User.findOne({email})
+        if(user) return res.status(400).json({message: "User already exists"});
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword
+        })
+
+        if(newUser){
+            //generate jwt token
+            
+        }else{
+            res.status(400).json({message:"Invalid User Data"});
+        }
+    } catch (error) {
+        
+    }
+};
+export const login = (req, res) => {
+    res.send('Login route');
+};
+
+export const logout = (req, res) => {
+    res.send('Logout route');
+};
+
