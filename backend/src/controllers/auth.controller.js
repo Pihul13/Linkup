29096bbb911ec9dbb81f0ba55878768b5b4b5b3a
Pihul Lalotra <pihullalotra23@gmail.com>
2025-06-15 17:46:25 +0530

    fixed signup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,10 +1,11 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 export const signup = async (req, res) => {
     const {fullName,email,password} = req.body
     try {
         //Hashing password using bcrypt
-        if(password < 6){
+        if(password.length < 6){
             return res.status(400).json({message: "Password must be atleast 6 characters."});
         }
 
@@ -22,12 +23,21 @@ export const signup = async (req, res) => {
 
         if(newUser){
             //generate jwt token
-            
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic:newUser.profilePic,
+            });
         }else{
             res.status(400).json({message:"Invalid User Data"});
         }
     } catch (error) {
-        
+        console.log("Error in signup controller:", error);
+        res.status(500).json({message: "Internal Server Error"});
     }
 };
 export const login = (req, res) => {
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,6 +8,10 @@ export const generateToken = (userId, res) => {
 
     res.cookie("jwt",token,{
         maxAge:7*24*60*60*1000,//millsecond
-        httpOnly:true,
-    })
-}
+        httpOnly:true,// Prevents XSS attacks by not allowing client-side JavaScript to access the cookie
+        sameSite:"strict", // Helps prevent CSRF attacks by ensuring the cookie is sent only in same-site requests
+        secure: process.env.NODE_ENV !== "development" ,// Ensures the cookie is sent over HTTPS in production
+    });
+
+    return token;
+};
