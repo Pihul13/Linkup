74ab531072a0dd58ab53153321b18b268bf720da
Pihul Lalotra <pihullalotra23@gmail.com>
2025-06-25 00:53:40 +0530

    auth middleware update

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -14,8 +14,15 @@ export const protectRoute = async (req,res,next) => {
             return res.status(401).json({message: "Unauthorised - Invalid Token"});
         }
 
-        const user = await User.findById(decoded.userId)
+        const user = await User.findById(decoded.userId).select("-password")
+
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+        req.user = user;
+        next();
     } catch (error) {
-        
+        console.log("Error in protectRoute middleware:", error.message);
+        return res.status(500).json({message: "Internal Server Error"});
     }
 }
