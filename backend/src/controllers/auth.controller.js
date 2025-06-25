088a2aca8a356e91867834eb1df382e2849bc78c
Pihul Lalotra <pihullalotra23@gmail.com>
2025-06-25 20:56:25 +0530

    updateProfile pic completed, and get method for check auth

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
@@ -75,5 +76,31 @@ export const logout = (req, res) => {
 };
 
 export const updateProfile = async (req, res) => {
-    
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic){
+            return res.status(400).json({message:"Profile Pic is required"});
+        }
+        // Upload the image to Cloudinary and save only the image URL (not the image itself) in the database.
+        // This keeps the database lightweight and leverages Cloudinary for efficient image storage and delivery.
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url},{new:True});
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.log("Error in updateProfile ", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+}
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller:", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+        
+    }
 }
