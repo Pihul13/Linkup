3d1825f4d0bd2f51f760354463ad3802b2b23af8
Pihul Lalotra <pihullalotra23@gmail.com>
2025-06-28 15:22:50 +0530

    message controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParse from 'cookie-parser'
+
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+
 import { connectDB } from './lib/db.js';
 
 dotenv.config()
@@ -13,6 +16,7 @@ app.use(express.json());
 app.use(cookieParse());
 
 app.use("/api/auth",authRoutes);
+app.use("/api/message",messageRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running on PORT:',+ PORT);
