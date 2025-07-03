0b78c7ddd0a5887de465bd146a4b9c803933f24a
Pihul Lalotra <pihullalotra23@gmail.com>
2025-07-03 23:07:13 +0530

    setup zustand and axios, learned about cors error and fixed it

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cookieParse from 'cookie-parser'
-
+import cookieParse from 'cookie-parser';
+import cors from 'cors';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
@@ -14,6 +14,10 @@ const PORT = process.env.PORT
 
 app.use(express.json());
 app.use(cookieParse());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true, // Allow cookies to be sent with requests
+}))
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
