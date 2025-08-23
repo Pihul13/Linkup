e9306d9dd2a5597c9b3d56c20b71bf9dcdc221d2
Pihul Lalotra <pihullalotra23@gmail.com>
2025-08-23 11:17:03 +0530

    created navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { axiosInstance } from './lib/axios'
 import { useAuthStore } from './store/useAuthstore'
 import { useEffect } from 'react'
+import { Toaster } from 'react-hot-toast'
 const App = () => {
   const {authUser, checkAuth,isCheckingAuth} = useAuthStore();
 
@@ -33,6 +34,8 @@ const App = () => {
         <Route path="/settings" element = {<SettingsPage />}/>
         <Route path="/profile" element = {authUser?<ProfilePage />: <Navigate to="/login" />}/>
       </Routes>
+
+      <Toaster/>
     </div>
   )
 }
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import {create} from 'zustand';
 import { axiosInstance } from '../lib/axios';
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
     authUser : null,
@@ -12,7 +13,6 @@ export const useAuthStore = create((set) => ({
     checkAuth: async()=>{
         try {
             const res = await axiosInstance.get("/auth/check");
-
             set({authUser:res.data});
         } catch (error) {
             console.log("Error in checking auth", error);
@@ -20,5 +20,30 @@ export const useAuthStore = create((set) => ({
         } finally{
             set({isCheckingAuth:false});
         }
-    }
+    },
+
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+        const res = await axiosInstance.post("/auth/signup", data);
+        set({ authUser: res.data });
+        toast.success("Account created successfully");
+        } catch (error) {
+        toast.error(error.response.data.message);
+        } finally {
+        set({ isSigningUp: false });
+        }
+    },
+
+    logout: async () => {
+        try {
+        await axiosInstance.post("/auth/logout");
+        set({ authUser: null });
+        toast.success("Logged out successfully");
+        } catch (error) {
+        toast.error(error.response.data.message);
+        }
+    },
+    
+
 }));
