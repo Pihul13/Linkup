258bbdeceed71157bec3ed1cdc9c52ba82daeaf4
Pihul Lalotra <pihullalotra23@gmail.com>
2025-08-23 11:37:57 +0530

    login page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { useAuthStore } from '../store/useAuthstore';
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User} from 'lucide-react';
 import { Link } from 'react-router-dom';
